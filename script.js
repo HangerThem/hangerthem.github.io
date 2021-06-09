9687295d7058446d54affc1ea91b4e027d82a3ae
HangerThem <35465244+HangerThem@users.noreply.github.com>
2021-06-09 21:27:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var min = today.getMinutes();
+  var n = today.getUTCDay();
+  var s = today.getSeconds();
+  var d = today.getDate();
+  var mon = today.getMonth();
+  var y = today.getFullYear();
+  if(n == 1) {
+    n = "MON";
+  } else if(n == 2)  {
+    n = "TUE";
+  } else if(n == 3)  {
+    n = "WED";
+  } else if(n == 4)  {
+    n = "THU";
+  } else if(n == 5)  {
+    n = "FRI";
+  } else if(n == 6)  {
+    n = "SAT";
+  } else if(n == 7)  {
+    n = "SUN";
+  }
+  min = checkTime(min);
+  s = checkTime(s);
+  document.getElementById('time').innerHTML =
+  h + ":" + min + ":" + s;
+   document.getElementById('date').innerHTML =
+  n + " " + d + "/" + mon + "/" + y;
+  var t = setTimeout(startTime, 1000);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
