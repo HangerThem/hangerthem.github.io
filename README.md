9bc3666a019a0ff308097af328c62bef708ea5d2
HangerThem <f.borisjuk@gmail.com>
2022-03-24 21:47:52 +0100

    Rebuild

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # hangerthem.github.io
 This project is my GitHub pages portfolio.
-<hr>
-This project was mainly started for me to learn basics of web devlopment.<br>
-It was built using HTML, CSS, JavaScript and little bit of PHP.<br>
+
+This project was mainly started for me to learn basics of web devlopment.
+
+It was built using HTML, CSS, JavaScript.
+
 I'm still working on it as I progress in understandig of web devlopment.
--- scripts/script.js
+++ scripts/script.js
@@ -1,58 +1,10 @@
-//Clock
-function startTime() {
-  setTimeout(startTime, 1000)
-
-  const today = new Date();
-  const timeFormater = new Intl.NumberFormat('en-us', {
-    minimumIntegerDigits: 2
-  })
-
-  var numberOfDay = today.getUTCDay()
-  var year = today.getFullYear()
-  var month = today.getMonth()
-  var day = today.getDate()
-  var hours = today.getHours()
-  var minutes = today.getMinutes()
-  var seconds = today.getSeconds()
-
-  //Choose name of day
-  function dayName(day) {
-    switch (day) {
-      case 0:
-        return 'SUN'
-      case 1:
-        return 'MON'
-      case 2:
-        return 'TUE'
-      case 3:
-        return 'WED'
-      case 4:
-        return 'THR'
-      case 5:
-        return 'FRI'
-      case 6:
-        return 'SAT'
-      default:
-        return ''
-    }
-  }
-
-  document.querySelector('[data-time]').innerHTML = `${hours}:${timeFormater.format(minutes)}:${timeFormater.format(seconds)}`
-  document.querySelector('[data-date]').innerHTML = `${dayName(numberOfDay)} ${day} / ${month} / ${year}`
-}
-
-//Social meadia alert
-function alertOpen() {
-  alert('Sorry, I don\'t want to link my poor social media here.')
-}
-
 //Nabvar dropdown
-const burgerButton = document.querySelector('[data-burger]')
-const navbar = document.querySelector('[data-navbar]')
-const cutter = document.querySelector('[data-cutter]')
 let burgerOpen = false
 
-burgerButton.addEventListener('click', () => {
+document.querySelector('[data-burger]').addEventListener('click', () => {
+  const burgerButton = document.querySelector('[data-burger]')
+  const navbar = document.querySelector('[data-navbar]')
+  const cutter = document.querySelector('[data-cutter]')
   if (!burgerOpen) {
     burgerButton.classList.add('open')
     navbar.style.display = 'flex'
@@ -69,19 +21,8 @@ burgerButton.addEventListener('click', () => {
 })
 
 //Dark/Light mode
-const switchButton = document.querySelector('[data-mode]')
-const root = document.documentElement
-let lightMode = false
-
-switchButton.addEventListener('click', () => {
-  if (!lightMode) {
-    root.style.setProperty('--primar', '#FFF')
-    root.style.setProperty('--white', '#000')
-    lightMode = true
-  }
-  else {
-    root.style.setProperty('--primar', '#000')
-    root.style.setProperty('--white', '#FFF')
-    lightMode = false
-  }
+document.querySelector('[data-mode]').addEventListener('click', () => {
+  const theme = document.querySelector('html').dataset.theme
+  if (theme === 'light') document.querySelector('html').dataset.theme = 'dark'
+  else document.querySelector('html').dataset.theme = 'light'
 })
